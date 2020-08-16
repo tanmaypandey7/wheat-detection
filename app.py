21f8cf0501f3b293c54588939f01d88b756840ba
Tanmay <tanmaypandey1998@gmail.com>
2020-08-17 00:26:25 +0530

    add waitress

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import request, render_template
 import os
 import shutil
 
-
 import config
 from utils.detect_utils import detect, save_image
+
+from waitress import serve
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -48,4 +49,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8080)
