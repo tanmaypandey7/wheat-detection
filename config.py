0fac0525ff9fc8908bac563ce7547dd4c3087a1d
Tanmay <tanmaypandey1998@gmail.com>
2020-08-17 00:17:55 +0530

    minor font changes to config

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-weights = "input/last_yolov5x_4M50fold0.pt"
-img_size = 256
-nms_conf_thr = 0.2
-nms_iou_thr = 0.9
-augment = True
-device = 'cpu'
-classes = None
-agnostic_nms = True
+WEIGHTS = "input/last_yolov5x_4M50fold0.pt"
+IMG_SIZE = 256
+NMS_CONF_THR = 0.2
+NMS_IOU_THR = 0.9
+AUGMENT = True
+CLASSES = None
+AGNOSTIC_NMS = True
+DEVICE = "cpu"
+SOURCE = "temp_image"
