06ac7357e3b5df6f6ed7b5bbc9624c0c4b85519a
Tanmay Pandey <tanmaypandey1998@gmail.com>
2020-09-09 12:39:14 +0530

    change default mode to dev

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def home():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080)
-    serve(app, host='0.0.0.0', port=8080)
+    app.run(port=8000)
+    # serve(app, host='0.0.0.0', port=8080)
